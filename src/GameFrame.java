c274489c87c08568b52a129d92a16104d465db92
Nihal Morshed <nilumorshed@gmail.com>
2022-05-17 21:13:33 +0600

    17 May 9:13

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,16 +1,15 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.*;
 import javax.swing.*;
 public class GameFrame extends JFrame{
+
     GamePanel panel;
 
-    public GameFrame() {
-        panel= new GamePanel();
+    GameFrame(){
+        panel = new GamePanel();
         this.add(panel);
         this.setTitle("Pong Game");
         this.setResizable(false);
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.black);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.pack();
         this.setVisible(true);
--- src/Paddle.java
+++ src/Paddle.java
@@ -2,84 +2,68 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 import javax.swing.*;
-public class Paddle extends Rectangle {
+public class Paddle extends Rectangle{
+
     int id;
     int yVelocity;
-    int speed=10;
+    int speed = 10;
 
-    public Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT,int id) {
+    Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
         super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
         this.id=id;
     }
-    public void keyPressed(KeyEvent e){
-        switch (id){
-            case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W){
-                    setYDirection(-speed);
-                    move();
-                }
 
-                if(e.getKeyCode()==KeyEvent.VK_S){
+    public void keyPressed(KeyEvent e) {
+        switch(id) {
+            case 1:
+                if(e.getKeyCode()==KeyEvent.VK_W) {
+                    setYDirection(-speed);
+                }
+                if(e.getKeyCode()==KeyEvent.VK_S) {
                     setYDirection(speed);
-                    move();
                 }
                 break;
             case 2:
-                if(e.getKeyCode()==KeyEvent.VK_NUMPAD8){
+                if(e.getKeyCode()==KeyEvent.VK_NUMPAD8) {
                     setYDirection(-speed);
-                    move();
                 }
-                if(e.getKeyCode()==KeyEvent.VK_NUMPAD5){
+                if(e.getKeyCode()==KeyEvent.VK_NUMPAD5) {
                     setYDirection(speed);
-                    move();
                 }
                 break;
-
         }
-
     }
-    public void keyReleased(KeyEvent e){
-        switch (id){
+    public void keyReleased(KeyEvent e) {
+        switch(id) {
             case 1:
-                if(e.getKeyCode()==KeyEvent.VK_W){
+                if(e.getKeyCode()==KeyEvent.VK_W) {
                     setYDirection(0);
-                    move();
                 }
-
-                if(e.getKeyCode()==KeyEvent.VK_S){
+                if(e.getKeyCode()==KeyEvent.VK_S) {
                     setYDirection(0);
-                    move();
                 }
                 break;
             case 2:
-                if(e.getKeyCode()==KeyEvent.VK_NUMPAD8){
+                if(e.getKeyCode()==KeyEvent.VK_NUMPAD8) {
                     setYDirection(0);
-                    move();
                 }
-                if(e.getKeyCode()==KeyEvent.VK_NUMPAD5){
+                if(e.getKeyCode()==KeyEvent.VK_NUMPAD5) {
                     setYDirection(0);
-                    move();
                 }
                 break;
-
         }
-
-
     }
-    public void setYDirection(int yDirection){
-        yVelocity= yDirection;
-
+    public void setYDirection(int yDirection) {
+        yVelocity = yDirection;
     }
-    public void move(){
-        y= y+yVelocity;
-
+    public void move() {
+        y= y + yVelocity;
     }
-    public void draw(Graphics g){
-        if(id == 1)
-            g.setColor(Color.BLUE);
-        else g.setColor(Color.red);
-        g.fillRect(x,y,width,height);
-
-
+    public void draw(Graphics g) {
+        if(id==1)
+            g.setColor(Color.blue);
+        else
+            g.setColor(Color.red);
+        g.fillRect(x, y, width, height);
     }
 }
